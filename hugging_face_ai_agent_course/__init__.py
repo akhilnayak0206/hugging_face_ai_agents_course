aecee303a41f66889b646fc5f6491163046bb8cb
Akhil Nayak <akhilnayak0206@gmail.com>
2026-04-03 18:10:17 +0530

    Update course materials and add unit 2

--- hugging_face_ai_agent_course/__init__.py
+++ hugging_face_ai_agent_course/__init__.py
@@ -1,5 +1,6 @@
 from unit1.dummy_ai_agent import dummy_ai_agent
 from unit1.first_smolagents import main as first_smolagents
+from unit2.try_imge import try_image
 
 
 def main():
@@ -9,7 +10,8 @@ def main():
     # dummy ai agent
     # dummy_ai_agent()
     # first smol agents
-    first_smolagents()
+    # first_smolagents()
+    try_image()
 
 
 if __name__ == "__main__":
